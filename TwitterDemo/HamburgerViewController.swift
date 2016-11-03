3c253f8c1034b8a6f20b1d968fddd62c2374dd8c
jason <bjason@gmail.com>
2016-11-03 14:14:48 -0700

    ghetto animation

---
+++ TwitterDemo/HamburgerViewController.swift
@@ -0,0 +1,59 @@
+//
+//  HamburgerViewController.swift
+//  TwitterDemo
+//
+//  Created by jason on 11/3/16.
+//  Copyright © 2016 jasonify. All rights reserved.
+//
+
+import UIKit
+
+class HamburgerViewController: UIViewController {
+
+    @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var contentView: UIView!
+    
+    var originalLeftMargin: CGFloat?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    @IBAction func onPanGesture(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        let velocity = sender.velocity(in: view)
+        
+        
+        if sender.state == UIGestureRecognizerState.began {
+            originalLeftMargin = leftMarginConstraint.constant
+        } else if sender.state == UIGestureRecognizerState.changed {
+            leftMarginConstraint.constant = originalLeftMargin! + translation.x
+        } else if sender.state == UIGestureRecognizerState.ended {
+            if velocity.x > 0 {
+                leftMarginConstraint.constant = view.frame.size.width - 50
+            } else {
+                leftMarginConstraint.constant = 0
+            }
+        }
+    }
+    
+    @IBOutlet weak var leftMarginConstraint: NSLayoutConstraint!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
